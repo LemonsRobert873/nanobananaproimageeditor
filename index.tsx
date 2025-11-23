73862c56aeb5cd6b0d4fdc6c6e8c20bdf0e812e7
LemonsRobert873 <lemonsrobert873@gmail.com>
2025-11-24 01:00:32 +0530

    feat: Add favicon to application
    Integrates the application's favicon for improved branding and user experience. The favicon is now linked in the HTML and the icon file has been added to the public directory.

--- index.tsx
+++ index.tsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// @ts-ignore - Ignores missing type definition for png
-import favicon from './banana.png';
-
-// Dynamically set favicon to ensure the bundler includes the file
-const setFavicon = (url: string) => {
-  let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
-  if (!link) {
-    link = document.createElement('link');
-    link.rel = 'icon';
-    document.head.appendChild(link);
-  }
-  link.href = url;
-};
-
-setFavicon(favicon);
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
